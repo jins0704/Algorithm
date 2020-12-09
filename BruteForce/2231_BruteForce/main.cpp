3454acd4ecea9f1dfb8b1b3af88bc5dc2fc3097c
홍진석 <hongjinseok@hongjinseog-ui-MacBook-Pro.local>
2020-12-10 02:22:23 +0900

    Solve

---
+++ BruteForce/2231_BruteForce/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  2231_BruteForce
+//
+//  Created by 홍진석 on 2020/07/17.
+//  Copyright © 2020 홍진석. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int g,ans=0;
+     int sum =0;
+    cin >> g;
+    for(int i=0; i<g; i++){
+        int n[6] = {0};
+        n[0] = (i%1000000-i%100000)/100000;
+        n[1] = (i%100000-i%10000)/10000;
+        n[2] = (i%10000-i%1000)/1000;
+        n[3] = (i%1000-i%100)/100;
+        n[4] = (i%100-i%10)/10;
+        n[5] = (i%10-i%1)/1;
+        sum = n[0]+n[1]+n[2]+n[3]+n[4]+n[5]+i;
+        if(sum==g){
+            ans =i;
+            break;
+        }
+    }
+    if(ans ==0) cout << 0;
+    else cout << ans;
+    return 0;
+}
