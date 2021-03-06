7f7e7327e861636e0cb8ffb2ee827fc5a1f46dbb
hongjinseok <sldkdl111@ajou.ac.kr>
2021-03-06 23:45:42 +0900

    add 16954

--- BFS,DFS/16954_BFS/16954_BFS/main.cpp
+++ BFS,DFS/16954_BFS/16954_BFS/main.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 int check[8][8][1000];
-// 8x8
 int arr[8][8];
 queue<pair<pair<int, int>,int>> loc;
 queue<pair<pair<int, int>,int>> wall;
@@ -14,6 +13,7 @@ queue<pair<pair<int, int>,int>> wall;
 bool finish = false;
 
 char c;
+
 // 인접한 칸 혹은 대각선 방향으로 이동하거나 서있는 것이 가능하다.
 int xx[8] = {-1,-1,-1,0,0,1,1,1};
 int yy[8] = {-1,0,1,-1,1,-1,0,1};
@@ -25,8 +25,7 @@ void Move(){
     check[7][0][0] = 1;
     
     while(!loc.empty()){
-        // 1초 마다 모둔 벽이 아래에 있는 행으로 한칸 내려간다. 행이 없다면 벽은 사라진다
-
+        //움직일 곳 찾기
         unsigned long lsize = loc.size();
     
         for(unsigned i=0; i < lsize; i++){
@@ -37,6 +36,7 @@ void Move(){
             loc.pop();
             
             if(arr[fx][fy] == 1 || ft != time){
+               // 다른 시간대이거나 벽을 옮기고 봤는데 벽이랑 같은 자리이면 패스
                // cout << fx << " " << fy << " " << ft << " 불가능"<< endl;
                 continue;}
             
@@ -64,7 +64,11 @@ void Move(){
             }
         }
         
+        int arr2[8][8] = {0};
+        
         if(!wall.empty()){
+            // 1초 마다 모둔 벽이 아래에 있는 행으로 한칸 내려간다. 행이 없다면 벽은 사라진다
+            
             
             unsigned long wsize = wall.size();
             
@@ -77,14 +81,19 @@ void Move(){
                 
                 //한칸씩 아래로 이동
                 if(fx <7 && fx >=0){
-                    arr[fx][fy] = 0;
-                    arr[fx+1][fy] = 1;
+                    arr2[fx+1][fy] = 1;
                     wall.push(make_pair(make_pair(fx+1, fy),ft+1));
                    // cout << fx+1 << " " << fy <<  " " << ft << " 벽 내려감"<< endl;
                 }
                 else{
                     arr[fx][fy] = 0;
                 }
+            }
+        }
+        
+        for(int q=0; q<8; q++){
+            for(int w =0; w<8; w++){
+                arr[q][w] = arr2[q][w];
             }
         }
         
